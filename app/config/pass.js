c5f2677f7d3b131d789d231bfa913949dda23096
KASolomon <98809297+KASolomon@users.noreply.github.com>
2024-09-10 11:50:45 +0000

    Update pass.js

--- app/config/pass.js
+++ app/config/pass.js
@@ -1,2 +1,4 @@
-export const weatherPass = "v08ryGnWaeIUlWrR7G0R8bXVqRsngdt5";
-export const geocodingPass = "9071ccac663349fa8d53a32bf096e7f4";
+//Supply an api key from tomorrow.io
+export const weatherPass = "";
+// Supply an api key from opencagedata.com
+export const geocodingPass = "";
